1db695e15107d18399da916b07400e5c2790bfb3
Mayra Romero <nahytar@gmail.com>
2018-05-27 19:10:36 -0400

    creando funciones de add y multiply

---
+++ assets/add.js
@@ -0,0 +1,9 @@
+// funcion suma, return devuelve el valor del calculo.
+function add(a, b) {
+    return a + b;
+}
+function multiply(a, b) {
+    return a * b;    
+}
+
+console.log(multiply(36325, add(9824, 777)))
